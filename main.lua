9a6ef061b5b9e6813e9e33cffeb2c83ddcb77b7d
Same Difference <keysoulv@live.com>
2011-11-11 15:06:18 -0800

    bullet count more logical. added bullet loop for updates. info updated.

--- main.lua
+++ main.lua
@@ -4,7 +4,10 @@ function love.load()
    x = 50
    y = love.graphics.getHeight() - player:getHeight()
    bullets = { {} }
-   bullet_count = 1
+   for i=1,60 do
+       bullets[i] = {}
+   end
+   bullet_count = 60
    is_reloading = 0
    speed = 100
 end
@@ -21,15 +24,19 @@ function love.update(dt)
        if is_reloading < 0 then
            print("done reloading")
            is_reloading = 0
-           bullet_count = 1
+           bullet_count = 60
        end
    end
 
    if love.keyboard.isDown(" ") then
       if is_reloading == 0 then
-         bullet_count = bullet_count + 1
+         bullet_count = bullet_count - 1
+         bullets[bullet_count] = {}
+         bullets[bullet_count].exists = 1
+         bullets[bullet_count].x = x
+         bullets[bullet_count].y = y 
          print("fire")
-         if bullet_count > 30 then
+         if bullet_count < 1 then
             is_reloading = 1
             print("reloading")
          end
@@ -42,10 +49,23 @@ function love.update(dt)
          print("reloading")
       end
    end
+
+   bullet_now = 0
+
+   while bullet_now <= 60 do
+      if bullets[bullet_now] ~= nil and bullets[bullet_now].exists == 1 then
+         bullets[bullet_now].y = bullets[bullet_now].y - (dt * 300)
+         if bullets[bullet_now].y < 0 then
+            bullets[bullet_now].exists = 0
+            print("bullet leaves")
+         end
+      end
+      bullet_now = bullet_now + 1
+   end
 end
 
 function love.draw()
    love.graphics.draw(player, x, y)
-   info = 30 - bullet_count + 1 .. "/" .. 30
+   info = bullet_count .. "/" .. 60 .. "\n" .. "Reloading: " .. is_reloading
    love.graphics.print(info, 0, 0)
 end
