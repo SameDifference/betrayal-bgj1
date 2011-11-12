99ad6fbb2caac24c213e4540cad163e6e2772141
Same Difference <keysoulv@live.com>
2011-11-11 18:43:33 -0800

    conversation system added.

--- main.lua
+++ main.lua
@@ -2,15 +2,40 @@ function love.load()
    love.graphics.setCaption("BaconGameJam 1")
    player = love.graphics.newImage("player.png")
    bullet = love.graphics.newImage("bullet.png")
+   jackson = love.graphics.newImage("jackson.png")
+   hall = love.graphics.newImage("jackson.png")
+   say_msg = {}
+   conversation = 0
    x = 50
    y = love.graphics.getHeight() - player:getHeight()
    bullets = { {} }
    for i=1,60 do
        bullets[i] = {}
    end
+
    bullet_count = 60
    is_reloading = 0
    speed = 100
+   conv = {
+      [1] = {"Delta X to Orange 3, do you copy?\n(Press s to skip!)", jackson},
+      [9] = {"Orange 3 copies loud and clear Delta X over.", hall},
+      [16] = {"Delta X to Orange 3 return time over.", jackson},
+      [25] = {"Orange 3 to Delta X it's 11:11 11/11/11, do you copy?", hall},
+      [32] = {"Delta X copies Orange 3, over.", jackson},
+      [40] = {"*shuts down the radio*", jackson},
+      [44] = {"I've spent so far, away from home.", jackson},
+      [52] = {"So sad, so alone...", jackson},
+      [61] = {"Delta X we have hostile movement in your area over!", hall},
+      [70] = {"Copy that Orange 3, will take them out.", jackson},
+      [80] = {nil, nil},
+      [130] = {"Delta X skies are clear.", hall},
+      [137] = {"But you, must die!", hall},
+      [145] = {"I knew it Hall!", jackson}, --_alarm},
+      [150] = {"It was not my order. But it must happen, die Jackson!", hall},
+      [170] = {"Where am I...", jackson} --_bloody}
+   }
+
+
 end
 
 function love.update(dt)
@@ -23,7 +48,7 @@ function love.update(dt)
    if is_reloading > 0 then
        is_reloading = is_reloading - dt
        if is_reloading < 0 then
-           print("done reloading")
+           --print("done reloading")
            is_reloading = 0
            bullet_count = 60
        end
@@ -36,18 +61,18 @@ function love.update(dt)
          bullets[bullet_count].exists = 1
          bullets[bullet_count].x = x + (player:getWidth() / 2)
          bullets[bullet_count].y = y 
-         print("fire")
+         --print("fire")
          if bullet_count < 1 then
             is_reloading = 1
-            print("reloading")
+            --print("reloading")
          end
       end
    end
 
    if love.keyboard.isDown("z") then
-      if is_reloading == 1 then
-         is_reloading = 1
-         print("reloading")
+      if is_reloading == 0 then
+         is_reloading = (.016666666) * ((bullet_count + 1) / 1.25)
+         --print("reloading")
       end
    end
 
@@ -58,11 +83,25 @@ function love.update(dt)
          bullets[bullet_now].y = bullets[bullet_now].y - (dt * 300)
          if bullets[bullet_now].y < 0 then
             bullets[bullet_now].exists = 0
-            print("bullet leaves")
+            --print("bullet leaves")
          end
       end
       bullet_now = bullet_now + 1
    end
+
+   conversation = conversation + dt
+   if love.keyboard.isDown("s") then
+      conversation = 70
+   end
+
+   if conv[math.floor(conversation)] ~= nil then
+      say_msg = conv[math.floor(conversation)]
+   end
+end
+
+function say(message, image)
+   love.graphics.draw(image, 0, love.graphics.getHeight() - image:getHeight())
+   love.graphics.print(message, image:getWidth(), love.graphics.getHeight() - image:getHeight())
 end
 
 function love.draw()
@@ -79,4 +118,7 @@ function love.draw()
       bullet_now = bullet_now + 1
    end
 
+   if say_msg[1] ~= nil then
+      say(say_msg[1], say_msg[2])
+   end
 end
